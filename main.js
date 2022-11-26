6f674e2b5bd110714490a76810a452bb6e48a225
Bholanath <bholanathbarik9748@gmail.com>
2022-11-26 13:07:49 +0530

    Design Pattern (This and fat arrow functions)

--- main.js
+++ main.js
@@ -1,31 +1,77 @@
-var obj = {
-    num : 2,
-    age : 1
-};
+"use strict";
 
-function add_No(e,f,g){
-    return this.age + e + f + g;
+// Function
+// var getA = function(params) {
+//     return params;
+// }
+
+// Fat Arrow
+let getA = a => a;
+
+console.log(getA(1));
+
+// Ex 2
+let square_of_a_no = (params) => {
+    return params * params
+};
+console.log(square_of_a_no(4));
+
+
+var x = function () {
+    var that = this;
+    this.val = 1;
+    setTimeout(function () {
+        that.val++;
+        console.log(that.val);
+    }, 1);
+}
+var XX = new x();
+
+var y = function () {
+    this.val = 1;
+    setTimeout(() => {
+        this.val++;
+        console.log(this.val);
+    }, 1);
 }
 
-// CALL
-console.log(add_No.call(obj,3,3,3));
+var yy = new y();
 
-// Apply
-var arr = [3,3,3];
-console.log(add_No.apply(obj,arr));
+var z = (arr) => {
+    console.log(arguments[0]);
+}
+z(1, 2, 3);
 
-// Bind
-console.log(add_No.bind(obj,3,3,3));
-var boud = add_No.bind(obj);
-console.log(boud(1,2,3));
 
-var Student = {
-    age : 20
-};
+// Assignment
+class Student {
+    constructor(name, age, phone_no, marks) {
+        this.name = name;
+        this.age = age;
+        this.phone_no = phone_no;
+        this.marks = marks;
+    }
 
-var print_name = (e) => {
-    return e + this.age;
+    static count_Student(){
+        return (Student.count++);
+    }
+
+    eligible(){
+        if(this.marks >= 40){
+            console.log(`${this.name} age ${this.age} is eligible`);
+        }else if(this.marks < 40){
+            console.log(`${this.name} age ${this.age} is not eligible . better luck next Time`);
+        }
+    }
 }
 
-var c = ['a','b','c'];
-console.log(Student.bind(obj));
+let s1 = new Student("naruto", 14,12344,98);
+let s2 = new Student("luffy", 15,42344,40);
+let s3 = new Student("goku", 16,42344,30);
+let s4 = new Student("asta", 17,42344,60);
+let s5 = new Student("L", 18,42344,54);
+s1.eligible();
+s2.eligible();
+s3.eligible();
+s4.eligible();
+s5.eligible();
